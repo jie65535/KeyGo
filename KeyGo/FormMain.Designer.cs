8a6fab9fa5dff961d874faff1626de2ddadec1e6
筱傑 <840465812@qq.com>
2021-04-28 13:12:24 +0800

    添加项目文件。

---
+++ KeyGo/FormMain.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace KeyGo
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtHotKey1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TxtHotKey1
+            // 
+            this.TxtHotKey1.BackColor = System.Drawing.Color.White;
+            this.TxtHotKey1.Location = new System.Drawing.Point(96, 122);
+            this.TxtHotKey1.Name = "TxtHotKey1";
+            this.TxtHotKey1.ReadOnly = true;
+            this.TxtHotKey1.Size = new System.Drawing.Size(213, 23);
+            this.TxtHotKey1.TabIndex = 0;
+            this.TxtHotKey1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TxtHotKey1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtHotKey1_KeyDown);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 315);
+            this.Controls.Add(this.TxtHotKey1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KeyGo!";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtHotKey1;
+    }
+}
+
---
+++ KeyGo/FormMain.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyGo
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+
+
+        void ProcessHotkey(int hotKey_id)
+        {
+            //if (hotKey_id == 100)
+            //    MessageBox.Show("Test");
+        }
+
+
+
+
+
+        private void TxtHotKey1_KeyDown(object sender, KeyEventArgs e)
+        {
+            TxtHotKey1.Text = e.Modifiers.ToString();
+            Console.WriteLine($"down\t{e.KeyData}");
+        }
+
+
+
+
+
+
+
+
+
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            //AppHotKey.RegKey(Handle, 100, AppHotKey.KeyModifiers.Ctrl, Keys.W);
+        }
+
+        private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //AppHotKey.UnRegKey(Handle, 100);
+        }
+
+
+
+        #region 窗体消息 热键回调
+        private const int WM_HOTKEY = 0x312;
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            switch (m.Msg)
+            {
+            case WM_HOTKEY:
+                ProcessHotkey(m.WParam.ToInt32());
+                break;
+            }
+        }
+        #endregion
+    }
+}
